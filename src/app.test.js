9c0dab619180bbdd31dc852e0264ebf9d0eff0ec
Jai Pal <jaipal.saini.0895@gmail.com>
2023-04-19 23:50:45 +0530

    Issue resove

--- src/app.test.js
+++ src/app.test.js
@@ -6,7 +6,7 @@ const request = supertest(app);
 
 describe("True", () => {
     it("Should", async()=>{
-        const res = await request.get("/test");
+        const res = await request.get("/");
         expect(res.status).toBe(200);
         expect(res.text).toBe("Hello world");
     });
